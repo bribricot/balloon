4dfc9589e44ffcb655b4b7af4f13c638907ec67c
LK <kraemer.lucie@gmail.com>
2021-08-17 04:35:53 +0200

    Pseudo code et débuts

---
+++ balloon.js
@@ -0,0 +1,32 @@
+/*
+function ChangeCouleur() {
+	while click simple 
+
+	if rouge passer à bleu
+		if bleu passer vert
+}
+
+
+function ClickEtAgrandit() {
+	for chaque click + 10px 
+	if height == 420px
+		revenir à 200px
+}
+
+function ClickOutsideEtDiminue () {
+		for chaque click - 5px 
+	if height == 200px
+		reste bloqué à 200px
+}
+
+function reverseColors {
+	while click outside
+		if bleu passer à green
+		if green passer à rouge
+}
+
+fonction clickOutside () {
+	if hover est en dehors
+		reverseColors
+}
+*/
