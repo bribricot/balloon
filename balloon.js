5b8ae1eb518366e793398484b6b0654e8255ad8a
bribricot <kraemer.lucie@gmail.com>
2021-08-22 22:56:10 +0200

    J'ai réussi! Maintenant je vais rendre tout ça plus beau

--- balloon.js
+++ balloon.js
@@ -1,32 +1,51 @@
-/*
-function ChangeCouleur() {
-	while click simple 
-
-	if rouge passer à bleu
-		if bleu passer vert
+function Height() {
+	return parseInt(document.getElementById("cercle").style.height);
+}
+function Width() {
+	return parseInt(document.getElementById("cercle").style.width);
 }
 
-
-function ClickEtAgrandit() {
-	for chaque click + 10px 
-	if height == 420px
-		revenir à 200px
+function ChangeWithClick() {
+	let element = document.getElementById("cercle");
+// Colors changes : R -> B -> G -> R (rouge bleu vert)
+	if (element.style.backgroundColor == "red")
+		element.style.backgroundColor = "blue";
+	else if (element.style.backgroundColor == "blue")
+		element.style.backgroundColor = "green";
+	else if (element.style.backgroundColor = "green")
+		element.style.backgroundColor = "red";
+// Le ballon grandit de 10 pixels jusqu'à atteindre 420 pixels puis le ballon explose et revient à sa taille initiale
+	if (Height() < 420) 
+		element.style.height = Height() + 10;
+	else 
+		element.style.height = "200px";
+	if (Width() < 420)
+		element.style.width = Width() + 10;
+	else 
+		element.style.width = "200px";
+	if (Height() == 425)
+		element.style.height = "200px";
+	if (Width() == 425)
+		element.style.width = "200px";
 }
 
-function ClickOutsideEtDiminue () {
-		for chaque click - 5px 
-	if height == 200px
-		reste bloqué à 200px
-}
+function mouseLeave() {
+	let element = document.getElementById("cercle"); 
+// Reverse colors : G -> B -> R -> G (vert bleu rouge)
+	if (element.style.backgroundColor == "green")
+		element.style.backgroundColor = "blue";
+	else if (element.style.backgroundColor == "blue")
+		element.style.backgroundColor = "red";
+	else if (element.style.backgroundColor = "red")
+		element.style.backgroundColor = "green";
+// Le ballon diminue de 5px puis stagne à 200px puis reste immobilisé à 200px
+	if (Height() > 200)
+		element.style.height = Height() - 5;
+	else 
+		element.style.height = "200px";
+	if (Width() > 200)
+		element.style.width = Width() - 5;
+	else 
+		element.style.height = "200px";
 
-function reverseColors {
-	while click outside
-		if bleu passer à green
-		if green passer à rouge
 }
-
-fonction clickOutside () {
-	if hover est en dehors
-		reverseColors
-}
-*/
