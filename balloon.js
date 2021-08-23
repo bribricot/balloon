012878a7a3751caec39f000ecd16bae45ab3f566
bribricot <kraemer.lucie@gmail.com>
2021-08-23 12:14:08 +0200

    Refactoring

--- balloon.js
+++ balloon.js
@@ -1,8 +1,5 @@
-function Height() {
+function Size() {
 	return parseInt(document.getElementById("cercle").style.height);
-}
-function Width() {
-	return parseInt(document.getElementById("cercle").style.width);
 }
 
 function ChangeWithClick() {
@@ -15,18 +12,18 @@ function ChangeWithClick() {
 	else if (element.style.backgroundColor = "green")
 		element.style.backgroundColor = "red";
 // Le ballon grandit de 10 pixels jusqu'à atteindre 420 pixels puis le ballon explose et revient à sa taille initiale
-	if (Height() < 420) 
-		element.style.height = Height() + 10;
-	else 
+	if (Size() < 420) {
+		element.style.height = Size() + 10;
+		element.style.width = Size() + 10;
+	}
+	else {
 		element.style.height = "200px";
-	if (Width() < 420)
-		element.style.width = Width() + 10;
-	else 
 		element.style.width = "200px";
-	if (Height() == 425)
+	}
+	if (Size() == 425) {
 		element.style.height = "200px";
-	if (Width() == 425)
 		element.style.width = "200px";
+	}
 }
 
 function mouseLeave() {
@@ -39,13 +36,13 @@ function mouseLeave() {
 	else if (element.style.backgroundColor = "red")
 		element.style.backgroundColor = "green";
 // Le ballon diminue de 5px puis stagne à 200px puis reste immobilisé à 200px
-	if (Height() > 200)
-		element.style.height = Height() - 5;
-	else 
+	if (Size() > 200) {
+		element.style.height = Size() - 5;
+		element.style.width = Size() - 5;
+	}
+	else {
 		element.style.height = "200px";
-	if (Width() > 200)
-		element.style.width = Width() - 5;
-	else 
-		element.style.height = "200px";
+		element.style.width = "200px";
+	}
 
 }
